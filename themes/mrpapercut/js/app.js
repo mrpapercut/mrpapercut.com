f1b852130032ad6f10b3883b91b4d31605e9a672
mrpapercut <mischa.rodermond@gmail.com>
2016-09-14 15:36:13 +0200

    Tweaks, added new post

--- themes/mrpapercut/js/app.js
+++ themes/mrpapercut/js/app.js
@@ -3,13 +3,22 @@
 require('../sass/app.scss');
 
 import 'core-js/fn/array/from';
+import 'core-js/fn/array/fill';
 
 import highlightSyntax from './util/highlight.js';
-// import obfuscate from './util/obfuscate.js';
+import obfuscate from './util/obfuscate.js';
 
 class App {
 	constructor() {
 		highlightSyntax();
+		this.fillBackground();
+	}
+
+	fillBackground() {
+		const code = new Array(15).fill(obfuscate('console.log("obfuscation is awesome")'));
+		const style = document.createElement('style');
+		style.innerHTML = 'body:before{content:"' + code.join() + '"}';
+		document.body.appendChild(style);
 	}
 }
 
